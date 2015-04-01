a4be5a9f474cd1a441ddbc81117d703ab169cd72
Andy Huggins <andrewhuggins@gmail.com>
2015-04-01 14:48:46 -0400

    adding the description to the theme for blog posts

--- src/views/clean/index.blade.php
+++ src/views/clean/index.blade.php
@@ -19,7 +19,7 @@
 @stop
 
 @section('main')
-    
+
     <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 @foreach( $posts as $post )
                 <div class="post-preview">
@@ -28,14 +28,14 @@
                             {{ $post->title }}
                         </h2>
                         <h3 class="post-subtitle">
-                            {{ $post->content }}
+                            {{ $post->description }}
                         </h3>
                     </a>
                     <p class="post-meta">Posted by <a href="#">{{ $post->user_id }}</a> on {{ date('F d, Y', strtotime($post->created_at)) }}</p>
                 </div>
                 <hr>
                 @endforeach
-                
+
                 <!-- Pager -->
                 {{ $posts->links('pagination::simple') }}
             </div>
