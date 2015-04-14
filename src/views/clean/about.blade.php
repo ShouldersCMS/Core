ab6abce7e19dc0fb67edb08ee7c009e6307030d1
Andy Huggins <andrewhuggins@gmail.com>
2015-04-14 11:01:59 -0400

    updating paths to assets for layout...had them set to local urls

--- src/views/clean/about.blade.php
+++ src/views/clean/about.blade.php
@@ -3,7 +3,7 @@
 @section('header')
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('http://shoulders.dev/img/about-bg.jpg')">
+    <header class="intro-header" style="background-image: url('{{ asset('packages/shoulderscms/shoulderscms/img/about-bg.jpg') }}')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
